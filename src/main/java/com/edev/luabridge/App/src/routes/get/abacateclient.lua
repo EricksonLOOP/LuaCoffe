2e93f063278b16f062397d6ed3246590e500ba3a
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2024-12-27 13:55:44 -0300

    feat/ Path file Search
    Agora ao requisitar um arquivo, não necessita mais estar dentro de get ou post, ele busca em todas as pastas do path configurado.

--- src/main/java/com/edev/luabridge/App/src/routes/get/abacateclient.lua
+++
@@ -1,3 +0,0 @@
-luaCoffe.mapping("get/creatingnewclient")
-local Luarequest = luacoffe.libs.LuaOkHttp.get('https://api.abacatepay.com/v1/customer/list', 'Bearer abc_dev_zL6eYHcxUZK4XMWzdZkXc0jS')
- return {code=200, response=Luarequest}
--- src/main/java/com/edev/luabridge/Modules/File/FileServicesImpl.java
+++ src/main/java/com/edev/luabridge/Modules/File/FileServicesImpl.java
@@ -7,7 +7,9 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 @Service
 public class FileServicesImpl implements FileServices, FilenameFilter {
@@ -20,19 +22,28 @@ public class FileServicesImpl implements FileServices, FilenameFilter {
     @Override
     public File encontrarArquivos(String name, String method) {
         String currentDirectory = Paths.get("").toAbsolutePath().toString();
-        File directory = new File(currentDirectory + filepath + method);
-        if (directory.exists() && directory.isDirectory()) {
-            File[] files = directory.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    if (file.getName().equals(name+".lua")) {
-                        return file;
-                    }
-                }
-            }
+        try{
+            Optional<Path> optionalPath = Files.walk(Paths.get(currentDirectory+filepath))
+                    .filter(Files::isRegularFile)
+                    .filter(p -> p.getFileName().toString().equals(name+".lua"))
+                    .findFirst();
+            return optionalPath.map(Path::toFile).orElse(null);
+        }catch(IOException e){
+            throw new RuntimeException(e.getCause());
         }
+//        File directory = new File(currentDirectory + filepath + method);
+//        if (directory.exists() && directory.isDirectory()) {
+//            File[] files = directory.listFiles();
+//            if (files != null) {
+//                for (File file : files) {
+//                    if (file.getName().equals(name+".lua")) {
+//                        return file;
+//                    }
+//                }
+//            }
+//        }
 
-        return null;
+//        return null;
     }
 
 
